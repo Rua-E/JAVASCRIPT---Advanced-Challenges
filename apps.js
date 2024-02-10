39bbe395cf7b58a642a36afb140537f49e84e4eb
Rua <rua.elkasheef@gmail.com>
2024-02-09 20:42:35 -0500

    completed JAVASCRIPT advanced challenges

--- apps.js
+++ apps.js
@@ -41,3 +41,34 @@ console.log(sortHighToLow([
     {id: 4, price: 10},
 ])
 );
+
+
+//CALL API
+
+async function postsByUser(userId){
+    
+    const promise = await fetch ("https://jsonplaceholder.typicode.com/posts/");
+    
+    const result = await promise.json()
+
+    const posts = result.filter(element => element.userId === userId)
+    
+    console.log(posts)
+}
+
+
+postsByUser(4);
+
+
+async function firstSixIncomplete(userId) {
+    const promise = await fetch ("https://jsonplaceholder.typicode.com/todos/");
+
+    const result = await promise.json()
+
+    const incompleteTasks = result.filter(elem => !elem.completed).slice(0,6);
+
+    console.log (incompleteTasks)
+}
+
+firstSixIncomplete(6)
+
